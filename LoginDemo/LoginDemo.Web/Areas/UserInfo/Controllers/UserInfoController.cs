48df8beccf044c8841241efabae6a2a580ac147d
jango2015 <jango2015>
2015-09-21 11:21:31 +0800

    add

---
+++ LoginDemo/LoginDemo.Web/Areas/UserInfo/Controllers/UserInfoController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using LoginDemo.BLL.Interface.UserAccount;
+using LoginDemo.Entity.UserAccount.QueryParameter;
+using LoginDemo.Web.Controllers;
+
+namespace LoginDemo.Web.Areas.UserInfo.Controllers
+{
+    public class UserInfoController : BaseController
+    {
+
+        #region properties
+        private readonly IUserAccountBLL _userAccountBll;
+        public IUserAccountBLL UserAccountBll
+        {
+            get { return _userAccountBll; }
+        }
+        #endregion
+
+        #region constructor
+        public UserInfoController(IUserAccountBLL iUserAccountBll)
+        {
+            _userAccountBll = iUserAccountBll;
+        }
+        #endregion
+        //
+        // GET: /UserInfo/UserInfo/
+        public ActionResult Index(UserInfoQueryParameter userInfoQueryParameter)
+        {
+            var data = UserAccountBll.Query(userInfoQueryParameter);
+            return View(data);
+        }
+
+
+        #region Register
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View("~/Areas/UserInfo/Views/UserInfo/_register.cshtml");
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Register(Entity.UserAccount.UserInfo userInfo)
+        {
+            var response = UserAccountBll.Register(userInfo);
+            return response.IsSuccess ? AlertSuccessJsonResult(response, "register success")
+                                      : AlertErrorJsonResult("", response.Message);
+        }
+
+        #endregion
+
+        #region Login
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View("~/Areas/UserInfo/Views/UserInfo/_login.cshtml");
+        }
+
+
+        [HttpPost]
+        public ActionResult Login(string Account, string Password)
+        {
+            var res = UserAccountBll.Login(new Entity.UserAccount.UserInfo() { Account = Account, Password = Password });
+            return res.IsSuccess ? AlertSuccessJsonResult("", "Login success") : AlertErrorJsonResult("", res.Message);
+        }
+
+        #endregion
+    }
+}
---
+++ LoginDemo/LoginDemo.Web/Areas/UserInfo/UserInfoAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace LoginDemo.Web.Areas.UserInfo
+{
+    public class UserInfoAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "UserInfo";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "UserInfo_default",
+                "UserInfo/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
