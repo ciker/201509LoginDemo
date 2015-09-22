22886513a1659eb95e74281f90a19fce2b26c117
jango2015 <jango2015>
2015-09-22 09:26:15 +0800

    test

--- LoginDemo/LoginDemo.BLL.Test/UserBLLTest.cs
+++ LoginDemo/LoginDemo.BLL.Test/UserBLLTest.cs
@@ -1,8 +1,11 @@
 ﻿using System;
-using LoginDemo.BLL.Interface;
+using LoginDemo.BLL.UserAccount;
 using LoginDemo.Commom;
 using LoginDemo.DAL;
+using LoginDemo.DAL.UserAccount;
 using LoginDemo.Entity;
+using LoginDemo.Entity.UserAccount;
+using LoginDemo.Entity.UserAccount.QueryParameter;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace LoginDemo.BLL.Test
@@ -10,7 +13,9 @@ namespace LoginDemo.BLL.Test
     [TestClass]
     public class UserBLLTest
     {
-        public UserBLL UserBll = new UserBLL(new UserDAL());
+        private readonly UserBLL _userBll = new UserBLL(new UserDAL());
+        private readonly UserAccountBLL _userAccountBll = new UserAccountBLL(new UserAccountDAL());
+        #region UserTest
         [TestMethod]
         public void UserRegister()
         {
@@ -22,14 +27,14 @@ namespace LoginDemo.BLL.Test
                 Mobile = "13678900123",
                 Email = "13678900123@qq.com"
             };
-            var response = UserBll.Register(user);
+            var response = _userBll.Register(user);
             Assert.AreEqual(true, response != null && response.IsSuccess);
         }
 
         [TestMethod]
         public void GetUserListbyParameterTestMethod()
         {
-            var ret = UserBll.GetUserListbyParameter(new UserQueryParameter());
+            var ret = _userBll.GetUserListbyParameter(new UserQueryParameter());
             Assert.AreEqual(true, ret.IsSuccess);
         }
 
@@ -37,12 +42,43 @@ namespace LoginDemo.BLL.Test
         public void LoginTestMethod()
         {
 
-            var ret = UserBll.Login(new User()
+            var ret = _userBll.Login(new User()
             {
                 UserName = "1_Unit_Test2",
                 UserPWD = "1_Unit_Test2",
             });
             Assert.AreEqual(false, ret.IsSuccess);
         }
+        #endregion
+
+        #region UserInfoTest
+        [TestMethod]
+        public void UserInfoRegister()
+        {
+            _userAccountBll.Register(new UserInfo()
+            {
+                Account = "UserInfo_Account_Test1",
+                Password = "UserInfo_Account_Test1",
+                CompanyName = "company 1",
+                Address = "address 1"
+            });
+        }
+
+        [TestMethod]
+        public void UserInfoLogin()
+        {
+            _userAccountBll.Login(new UserInfo()
+            {
+                Account = "UserInfo_Account_Test1",
+                Password = "UserInfo_Account_Test1"
+            });
+        }
+
+        [TestMethod]
+        public void UserInfoQuery()
+        {
+            _userAccountBll.Query(new UserInfoQueryParameter());
+        }
+        #endregion
     }
 }
