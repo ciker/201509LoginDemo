6740f972bea64f8f49c32de307ce447a23a57bb9
jango2015 <jango2015>
2015-09-23 16:23:41 +0800

    add

--- LoginDemo/LoginDemo.Web/Global.asax.cs
+++ LoginDemo/LoginDemo.Web/Global.asax.cs
@@ -29,10 +29,45 @@ namespace LoginDemo.Web
             //var currentDomin = AppDomain.CurrentDomain;
             //currentDomin.AssemblyResolve += CurrentDominOnAssemblyResolve;
 
+            //autofac文档：属性注入
+
+            //    AutoFac文档
+            //    目录
+            //    开始
+            //    Registering components
+            //    控制范围和生命周期
+            //    用模块结构化Autofac
+            //    xml配置
+            //    与.net集成
+            //    深入理解Autofac
+            //    指导
+            //    关于
+            //    词汇表
+            //    属性注入
+            //    属性注入使用可写属性而不是构造函数参数实现注入。
+
+            //    介绍
+            //    如果component是一个委托，使用一个对象初始化：
+
+            //    1
+            //    builder.Register(c => new A { B = c.Resolve<B>() });
+            //    为了提供循环依赖（就是当A使用B的时候B已经初始化），需要使用OnActivated事件接口：
+
+            //    1
+            //    builder.Register(c => new A()).OnActivated(e => e.Instance.B = e.Context.Resolve<B>());
+            //    通过发射，使用PropertiesAutowired（）修饰符注入属性。
+
+            //    1
+            //    builder.RegisterType<A>().PropertiesAutowired();
+            //    如果你预先知道属性的名字和值，你可以使用
+
+            //    1
+            //    builder.WithProperty("propertyName", propertyValue)。
             #region builder container
 
             var assemblies = AppDomain.CurrentDomain.GetAssemblies();
             builder.RegisterControllers(assemblies.ToArray());
+            
             builder.RegisterAssemblyTypes(assemblies).Where(t => baseType.IsAssignableFrom(t) && t != baseType)
                 .AsImplementedInterfaces().InstancePerLifetimeScope();
 
