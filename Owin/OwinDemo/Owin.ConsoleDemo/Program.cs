613b2fe8876f782a5555cac01439f2ea26815e95
jango2015 <jango2015>
2015-10-15 10:21:43 +0800

    add compute file hashMD5 code

--- Owin/OwinDemo/Owin.ConsoleDemo/Program.cs
+++ Owin/OwinDemo/Owin.ConsoleDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net.Http;
 using Microsoft.Owin.Hosting;
 
@@ -8,25 +9,52 @@ namespace Owin.ConsoleDemo
     {
         static void Main(string[] args)
         {
-            const string url = "http://localhost:8080/";
-            var startOpts = new StartOptions(url)
-            {
-            };
-            //using (WebApp.Start<Startup>(startOpts))
+            #region file hashcode
+
+            var fpath = @"D:\sysbacke450.GHO";
+            var file = new FileInfo(fpath);
+            var hash = file.GetHashCode();
+            var MD5Hash = HashHelper.ComputeMD5(fpath);
+
+            var copyPath = @"D:\sysbacke4501123123.GHO";
+            file.CopyTo(copyPath);
+            var fileC = new FileInfo(copyPath);
+            var hashCopy = fileC.GetHashCode();
+            var MD5HashC = HashHelper.ComputeMD5(copyPath);
+
+            Console.WriteLine(hash);
+
+            Console.WriteLine(MD5Hash);
+            Console.WriteLine(hashCopy);
+            Console.WriteLine(MD5HashC);
+            Console.ReadLine();
+
+            #endregion
+
+
+            #region webapi owin self host
+
+            //const string url = "http://localhost:8080/";
+            //var startOpts = new StartOptions(url)
             //{
-            //    Console.WriteLine("Server run at " + url + " , press Enter to exit.");
+            //};
+            ////using (WebApp.Start<Startup>(startOpts))
+            ////{
+            ////    Console.WriteLine("Server run at " + url + " , press Enter to exit.");
+            ////    Console.ReadLine();
+            ////}
+            //using (WebApp.Start<Startup>(url: url))
+            //{
+            //    var client = new HttpClient();
+            //    var response = client.GetAsync(url + "api/HomeApi/1").Result;
+
+            //    Console.WriteLine(response);
+            //    Console.WriteLine(response.Content.ReadAsStringAsync().Result);
             //    Console.ReadLine();
+
             //}
-            using (WebApp.Start<Startup>(url: url))
-            {
-                var client = new HttpClient();
-                var response = client.GetAsync(url + "api/HomeApi/1").Result;
 
-                Console.WriteLine(response);
-                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
-                Console.ReadLine();
-
-            }
+            #endregion
         }
     }
 }
